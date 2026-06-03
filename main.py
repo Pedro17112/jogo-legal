bca1ec3d6ad359e2fc45300111f5aa849d177745
Pedro17112 <pedrogarrocholeao1711@gmail.com>
2026-06-03 20:19:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_random_number():
+    return random.randint(1, 100)
+if __name__ == "__main__":
+    random_number = generate_random_number()
+    print(f"Generated random number: {random_number}")  
+
+
+
